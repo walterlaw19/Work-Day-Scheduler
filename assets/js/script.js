df0f5f5a3a10c36c21b64451011cf308d89b1cda
SC2021 <walterlaw19@gmail.com>
2021-07-23 11:03:56 -0400

    update time and fix div/text/button in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 // var todayIs = document.querySelector("#currentDay");
 
+function updateTime() {
+    currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").text(currentTime);
+}
 
-
-var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+updateTime()
 console.log(currentTime.toString());
 
 var TimeInterval = setInterval(function ()  {
-    
-    $("#currentDay").text(currentTime);
+    updateTime()
 },1000);
+
+
+
+function hourTracker() {
+    var currentHour = moment().hour();
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(blockHour, currentHour)
+    })
+}
+
+hourTracker();
+
+
 
 
 
@@ -16,3 +32,17 @@ var TimeInterval = setInterval(function ()  {
 
 
 // $("#errand").on("click") 
+
+const hourIds = [
+    "7a", "8a", "9a", "10a"
+]
+
+for (let index = 0; index < hourIds.length; index++) {
+    $('#btn-' + hourIds[index]).on('click', function(event) {
+        console.log($(event.target).siblings()[1].value)
+        // set to localstorage
+        localStorage.setItem(hourIds[index], $(event.target).siblings()[1].value)
+    });
+}
+
+
