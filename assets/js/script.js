c96493044dac5b3279ade4e0ee5033f6e7497099
SC2021 <walterlaw19@gmail.com>
2021-07-23 09:28:08 -0400

    add current day and time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
-var todayIs = document.querySelector("#currentDay");
+// var todayIs = document.querySelector("#currentDay");
 
 
 
-
-
-
-
-var currentTime = moment().format('MMMM Do YYYY');
-
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(currentTime.toString());
 
+var TimeInterval = setInterval(function ()  {
+    
+    $("#currentDay").text(currentTime);
+},1000);
 
+
+
+// $("#currentDay").text(currentTime);
 
 
 // $("#errand").on("click") 
