e1a4ffc23b127ddc4e930e836b532bffc60073b2
SC2021 <walterlaw19@gmail.com>
2021-07-23 08:48:25 -0400

    blocks inserted

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var todayIs = document.querySelector("#currentDay");
+
+
+
+
+
+
+
+var currentTime = moment().format('MMMM Do YYYY');
+
+console.log(currentTime.toString());
+
+
+
+
+// $("#errand").on("click") 
